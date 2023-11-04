bd384b9ff8befed13622e51b2d6358838cbd288a
ndelanhese <nnathanh3@gmail.com>
2023-11-04 20:04:21 -0300

    refactor(products): added cache revalidate

--- src/app/(website)/api.ts
+++ src/app/(website)/api.ts
@@ -2,5 +2,8 @@ import { Product } from '@global-types/products'
 import { api } from '@api'
 
 export const getFeaturedProducts = async () => {
-  return await api<Product[]>('/products/featured')
+  const ONE_HOUR_IN_SECONDS = 3600
+  return await api<Product[]>('/products/featured', {
+    next: { revalidate: ONE_HOUR_IN_SECONDS },
+  })
 }
